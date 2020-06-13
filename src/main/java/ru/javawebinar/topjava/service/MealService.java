76dcfb48f9cb39478cbf8501a9d20c38c677bdf4
Evgeni Panferov <glassesnow@yandex.ru>
2020-06-13 16:23:49 +0300

    MealService(add logic), MealServlet(insert authUserId in param).

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,40 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.web.SecurityUtil;
+
+import java.util.Collection;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 public class MealService {
 
     private MealRepository repository;
+    private int userId;
 
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+        userId = SecurityUtil.authUserId();
+    }
+
+    public Meal create(Meal meal) {
+        return repository.save(meal, userId);
+    }
+
+    public void delete(int id) {
+        checkNotFoundWithId(repository.delete(id, userId), id);
+    }
+
+    public Meal get(int id) {
+        return checkNotFoundWithId(repository.get(id, userId), id);
+    }
+
+    public Collection<Meal> getAll() {
+        return repository.getAll(userId);
+    }
+
+    public void update(Meal meal){
+        checkNotFoundWithId(repository.save(meal,userId),meal.getId());
+    }
 }
