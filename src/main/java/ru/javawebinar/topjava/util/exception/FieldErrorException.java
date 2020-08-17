051df3e7c30201f332afc838c4ac95542f09ea8d
Evgeni Panferov <glassesnow@yandex.ru>
2020-08-17 20:48:09 +0300

    Admin/Meal -UIController validation through ExceptionInfoHandler

---
+++ src/main/java/ru/javawebinar/topjava/util/exception/FieldErrorException.java
@@ -0,0 +1,7 @@
+package ru.javawebinar.topjava.util.exception;
+
+public class FieldErrorException extends RuntimeException{
+    public FieldErrorException(String msg) {
+        super(msg);
+    }
+}
